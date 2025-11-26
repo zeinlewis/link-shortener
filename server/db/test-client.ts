51649bdb80b91af5f9d817d8ed4466df68a1c27c
Said Babaiev <babaiev.info@gmail.com>
2025-11-26 22:26:00 +0100

    fix

--- server/db/test-client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client"
-import { drizzle } from "drizzle-orm/libsql"
-
-const turso = createClient({
-	url: ":memory:",
-})
-
-export const testDb = drizzle(turso)
